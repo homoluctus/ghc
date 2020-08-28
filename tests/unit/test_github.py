669b769fd841e702ed5cb04872f638ea7890668d
homoluctus <w.slife18sy@gmail.com>
2020-08-29 05:44:50 +0900

    test: Add a unit test for github.SearchQueryString

--- tests/unit/test_github.py
+++ tests/unit/test_github.py
@@ -1,4 +1,8 @@
-from ghc.github import Repository
+from typing import List, Optional
+
+import pytest
+
+from ghc.github import Repository, SearchQueryString
 from tests.utils import load_json
 
 
@@ -9,3 +13,31 @@ def test_repository():
     actual_result = [Repository(**x).to_dict() for x in source]
 
     assert actual_result == expectation
+
+
+@pytest.mark.parametrize(
+    'owner, topics, expectation',
+    (
+        (
+            'homoluctus',
+            ['python'],
+            'user:homoluctus topic:python'
+        ),
+        (
+            'homoluctus',
+            ['python', 'golang'],
+            'user:homoluctus topic:python topic:golang'
+        ),
+        (
+            'homoluctus',
+            None,
+            'user:homoluctus'
+        ),
+    )
+)
+def test_search_query_string(
+        owner: str,
+        topics: Optional[List[str]],
+        expectation: str):
+    q = SearchQueryString(owner, topics).to_string()
+    assert q == expectation
